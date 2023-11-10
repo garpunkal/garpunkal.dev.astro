5e5cb6dbf3a58476292495c1e6e3270af12ed492
Gareth Wright <garpunkal@gmail.com>
2023-11-10 19:28:29 +0000

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,27 +3,24 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import favicons from "astro-favicons";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://garpunkal.dev',
-  integrations: [
-    tailwind(),
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        'https://garpunkal.dev/sitemap-index.xml']
-    }),
-    favicons({
-      masterPicture: "public/favicon.png",
-      emitAssets: true,
-      appName: "garpunkal.dev",
-      appShortName: "garpunkal.dev",
-      appDescription: "garpunkal.dev",
-      lang: "en-GB",
-      background: "#fff",
-      theme_color: "#fff",
-      faviconsDarkMode: false,
-    })
-  ]
+  integrations: [tailwind(), sitemap(), robotsTxt({
+    sitemap: ['https://garpunkal.dev/sitemap-index.xml']
+  }), favicons({
+    masterPicture: "public/favicon.png",
+    emitAssets: true,
+    appName: "garpunkal.dev",
+    appShortName: "garpunkal.dev",
+    appDescription: "garpunkal.dev",
+    lang: "en-GB",
+    background: "#fff",
+    theme_color: "#fff",
+    faviconsDarkMode: false
+  })],
+  output: "server",
+  adapter: vercel()
 });
