f6fea63d17508d1e0e0e80d7a35c807faae995b5
Gareth Wright <garpunkal@gmail.com>
2023-11-10 19:29:25 +0000

    updated vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://garpunkal.dev',
+  prefetch: true,
   integrations: [tailwind(), sitemap(), robotsTxt({
     sitemap: ['https://garpunkal.dev/sitemap-index.xml']
   }), favicons({
