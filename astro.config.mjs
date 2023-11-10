c29defd6be6a62463387a2f68a264fc5448cdecc
Gareth Wright <garpunkal@gmail.com>
2023-11-10 19:39:01 +0000

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,17 +8,21 @@ import favicons from "astro-favicons";
 export default defineConfig({
   site: 'https://garpunkal.dev',
   prefetch: true,
-  integrations: [tailwind(), sitemap(), robotsTxt({
-    sitemap: ['https://garpunkal.dev/sitemap-index.xml']
-  }), favicons({
-    masterPicture: "public/favicon.png",
-    emitAssets: true,
-    appName: "garpunkal.dev",
-    appShortName: "garpunkal.dev",
-    appDescription: "garpunkal.dev",
-    lang: "en-GB",
-    background: "#077da0",
-    theme_color: "#077da0",
-    faviconsDarkMode: true
-  })]  
+  integrations: [
+    tailwind(),
+    sitemap(),
+    robotsTxt({
+      sitemap: ['https://garpunkal.dev/sitemap-index.xml']
+    }),
+    favicons({
+      masterPicture: "public/favicon.png",
+      emitAssets: true,
+      appName: "garpunkal.dev",
+      appShortName: "garpunkal.dev",
+      appDescription: "garpunkal.dev",
+      lang: "en-GB",
+      background: "#077da0",
+      theme_color: "#077da0",
+      faviconsDarkMode: true
+    })]
 });  
